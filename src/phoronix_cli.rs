e49674a695f47297f0618637d14001e47c5c0b43
jim4067 <jimmyimpulse2@gmail.com>
2021-05-05 20:18:24 +0300

    feat: change color for cli terminal display

--- src/phoronix_cli.rs
+++ src/phoronix_cli.rs
@@ -5,8 +5,8 @@ use term;
 
 #[allow(dead_code)]
 pub fn print() {
-    let phoronix_articles = Article::get_articles(&homepage::online()); //online// 
-    // let phoronix_articles = Article::get_articles(homepage::offline()); //offline
+    let phoronix_articles = Article::get_articles(&homepage::online()); //online//
+                                                                        // let phoronix_articles = Article::get_articles(homepage::offline()); //offline
 
     for article in phoronix_articles {
         println!(" Title: {}", article.title); //how to fix the numerous tabs and line breaks
@@ -26,7 +26,7 @@ pub fn print_coloured() {
 
     for article in phoronix_articles.iter().rev() {
         print!("Title:   ");
-        terminal.fg(term::color::BRIGHT_RED).unwrap();
+        terminal.fg(term::color::BRIGHT_YELLOW).unwrap();
         terminal.attr(term::Attr::Bold).unwrap();
         println!("{}", article.title);
         terminal.reset().unwrap();
@@ -35,10 +35,17 @@ pub fn print_coloured() {
         terminal.fg(term::color::BRIGHT_GREEN).unwrap();
         println!("https://www.phoronix.com/{}", article.link);
         terminal.reset().unwrap();
-        println!("Details: {}\nSummary:", article.details);
 
+        print!("Details: ");
+        terminal.fg(term::color::MAGENTA).unwrap();
+        terminal.attr(term::Attr::Bold).unwrap();
+        println!("{}", article.details);
+        terminal.reset().unwrap();
+
+        print!("\nSummary:");
         for line in linesplit::split_by_chars(&article.summary, 77).iter() {
             print!(" - ");
+            terminal.fg(term::color::BRIGHT_CYAN).unwrap();
             terminal.attr(term::Attr::Bold).unwrap();
             println!("{}", line);
             terminal.reset().unwrap();
